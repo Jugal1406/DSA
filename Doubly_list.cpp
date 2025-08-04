36a4bc6cd22892299420d2ea42ad38c16b0b29dd
jugal kshatriya <jugalkshatriya9@gmail.com>
2025-08-04 20:29:08 +0530

    node created

---
+++ Doubly_list.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node* prev;
+    node(int val)
+    {
+        data=val;
+        next=nullptr;
+        prev=nullptr;
+    }
+};
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
