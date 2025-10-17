7f2f3913227bb2c3464bd0fdd85ec6d99064f1f0
Jugal1406 <jugalkshatriya9@gmail.com>
2025-10-17 16:55:16 +0530

    all patterns done

---
+++ Practice/Patterns/sequence_num_right_tri.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            sum++;
+            cout<<sum;
+        }
+        cout<<endl;
+    }
+}
